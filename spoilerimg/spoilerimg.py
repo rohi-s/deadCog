aa9eb22c1dabcb3e06fb41bec94a1febcd77dcf6
rohit <63877756+rohi-s@users.noreply.github.com>
2021-10-09 16:32:31 +0530

    Update spoilerimg.py

--- spoilerimg/spoilerimg.py
+++ spoilerimg/spoilerimg.py
@@ -77,7 +77,7 @@ class spoilerimg(commands.Cog):
         ):
             await channel.send("You didn't attach any images or videos for me to spoil.")
         else:
-            webhook = await channel.create_webhook(name=message.author.display_name,avatar=message.author.avatar_url)
+            webhook = await channel.create_webhook(name=message.author.display_name)
             await self.sendhookEngine(webhook, message, textmessage, message.author.display_name, message.author.avatar_url)
             await message.delete(delay=0)
             await webhook.delete()
@@ -148,7 +148,7 @@ class spoilerimg(commands.Cog):
         else:
             channel = message.channel
 
-            webhook = await channel.create_webhook(name=message.author.display_name,avatar=message.author.avatar_url)
+            webhook = await channel.create_webhook(name=message.author.display_name)
             await self.sendhookEngine(webhook, message, textmessage, message.author.display_name, message.author.avatar_url)
             await message.delete(delay=0)
             await webhook.delete()
